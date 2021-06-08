0149f9187f20a387e4391e4d2df007808c37921f
Hassen Hassen <xasansq@gmail.com>
2021-06-08 12:38:10 -0500

    added functionality

---
+++ app.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('#input')
+let ul = document.querySelector('#ul')
+
+
+save = []
+
+input.addEventListener('keydown', function (e) {
+    if (e.code === 'Enter') {
+        let li = document.createElement('li')
+        li.classList.add('liStyle')
+        li.innerText = input.value
+        ul.appendChild(li)
+        input.value = ''
+        li.addEventListener('click', function () {
+            ul.removeChild(li)
+        })
+    }
+})
+
+
